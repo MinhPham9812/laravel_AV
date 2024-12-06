9b66d2c2c926e6fdebfff0281335177bb797fdd2
Minh Pham <minh9812@gmail.com>
2024-12-06 00:35:05 -0500

    update header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,7 +9,7 @@
             <!-- Logo -->
             <a href="{{ url('/') }}">
                 <div class="header__logo">
-                    <img src="assets/images/logo_Aking.svg" alt="logo" class="header__logo-img">
+                    <img alt="logo" class="header__logo-img" src="assets/images/logo_Aking.svg">
                     <div class="header__logo-text">
                         <span class="header__logo-text--aking">AKING</span>
                         <span class="header__logo-text--village">VILLAGE</span>
@@ -20,22 +20,27 @@
             <!-- Main navigation menu -->
             <nav class="header__nav">
                 <ul class="header__menu">
-                    <li class="header__menu-item"><a href="{{ url('/aking-general') }}" class="header__menu-link">ANKING VILLAGE</a></li>
-                    <li class="header__menu-item"><a href="{{ url('/pineapple-village-model') }}" class="header__menu-link">DỰ ÁN LÀNG DỨA</a></li>
-                    <li class="header__menu-item"><a href="{{ url('/investment-farm')}}" class="header__menu-link">ĐẦU TƯ ĐẤT FARM</a></li>
-                    <li class="header__menu-item"><a href="{{ url('/investment-seminar') }}" class="header__menu-link">HỘI THẢO ĐẦU TƯ</a></li>
-                    <li class="header__menu-item"><a href="{{ url('/news') }}" class="header__menu-link">TIN TỨC</a></li>
-                    <li class="header__menu-item"><a href="" class="header__menu-link">Q&A</a></li>
-                    <li class="header__menu-item"><a href="" class="header__menu-link">CONTACT</a></li>
+                    <li class="header__menu-item"><a class="header__menu-link" href="{{ url('/aking-general') }}">ANKING
+                            VILLAGE</a></li>
+                    <li class="header__menu-item"><a class="header__menu-link"
+                            href="{{ url('/pineapple-village-model') }}">DỰ ÁN LÀNG DỨA</a></li>
+                    <li class="header__menu-item"><a class="header__menu-link" href="{{ url('/investment-farm') }}">ĐẦU
+                            TƯ ĐẤT FARM</a></li>
+                    <li class="header__menu-item"><a class="header__menu-link"
+                            href="{{ url('/investment-seminar') }}">HỘI THẢO ĐẦU TƯ</a></li>
+                    <li class="header__menu-item"><a class="header__menu-link" href="{{ url('/news') }}">TIN TỨC</a>
+                    </li>
+                    <li class="header__menu-item"><a class="header__menu-link" href="">Q&A</a></li>
+                    <li class="header__menu-item"><a class="header__menu-link" href="">CONTACT</a></li>
                 </ul>
             </nav>
 
             <!-- Social icons -->
             <div class="header__social">
-                <a href="" class="header__social-icon">f</a>
+                <a class="header__social-icon" href="">f</a>
                 <div class="header__social-language">
-                    <a href="" class="header__social-lang header__social-lang--vi">VI</a>
-                    <a href="" class="header__social-lang header__social-lang--en">EN</a>
+                    <a class="header__social-lang header__social-lang--vi" href="">VI</a>
+                    <a class="header__social-lang header__social-lang--en" href="">EN</a>
                 </div>
             </div>
 
@@ -43,30 +48,34 @@
             <div class="header__search">
                 <i class="fa-solid fa-magnifying-glass"></i>
             </div>
-        </div>    
+        </div>
     </div>
-     <!-- Sidebar Structure -->
-     <div class="header__overlay"></div> <!-- Overlay, hiển thị khi mở sidebar -->
-            
-     <div class="header__mobile">
-         <span class="header__mobile-close">&times;</span> <!-- Nút đóng sidebar -->
-         <ul class="header__mobile-menu">
-             <li class="header__mobile-menu-item"><a href="#" class="header__mobile-menu-link">ANKING VILLAGE</a></li>
-             <li class="header__mobile-menu-item"><a href="#" class="header__mobile-menu-link">DỰ ÁN LÀNG DỨA</a></li>
-             <li class="header__mobile-menu-item"><a href="#" class="header__mobile-menu-link">ĐẦU TƯ FARM</a></li>
-             <li class="header__mobile-menu-item"><a href="#" class="header__mobile-menu-link">HỘI THẢO ĐẦU TƯ</a></li>
-             <li class="header__mobile-menu-item"><a href="#" class="header__mobile-menu-link">TIN TỨC</a></li>
-             <li class="header__mobile-menu-item"><a href="#" class="header__mobile-menu-link">Q&A</a></li>
-             <li class="header__mobile-menu-item"><a href="#" class="header__mobile-menu-link">CONTACT</a></li>
-             
-             <div class="header__mobile-social">
-                 <a href="" class="header__mobile-social-icon">f</a>
-                 <a href="#" class="header__mobile-lang header__mobile-lang--active">VI</a>
-                 <a href="#" class="header__mobile-lang">EN</a>
-             </div>
-         </ul>
-     </div>
+    <!-- Sidebar Structure -->
+    <div class="header__overlay"></div> <!-- Overlay, hiển thị khi mở sidebar -->
+
+    <div class="header__mobile">
+        <span class="header__mobile-close">&times;</span> <!-- Nút đóng sidebar -->
+        <ul class="header__mobile-menu">
+            <li class="header__mobile-menu-item"><a class="header__mobile-menu-link"
+                    href="{{ url('/aking-general') }}">ANKING VILLAGE</a></li>
+            <li class="header__mobile-menu-item"><a class="header__mobile-menu-link"
+                    href="{{ url('/pineapple-village-model') }}">DỰ ÁN LÀNG DỨA</a></li>
+            <li class="header__mobile-menu-item"><a class="header__mobile-menu-link"
+                    href="{{ url('/investment-farm') }}">ĐẦU TƯ FARM</a></li>
+            <li class="header__mobile-menu-item"><a class="header__mobile-menu-link"
+                    href="{{ url('/investment-seminar') }}">HỘI THẢO ĐẦU TƯ</a></li>
+            <li class="header__mobile-menu-item"><a class="header__mobile-menu-link" href="{{ url('/news') }}">TIN
+                    TỨC</a></li>
+            <li class="header__mobile-menu-item"><a class="header__mobile-menu-link" href="#">Q&A</a></li>
+            <li class="header__mobile-menu-item"><a class="header__mobile-menu-link" href="#">CONTACT</a></li>
+
+            <div class="header__mobile-social">
+                <a class="header__mobile-social-icon" href="">f</a>
+                <a class="header__mobile-lang header__mobile-lang--active" href="#">VI</a>
+                <a class="header__mobile-lang" href="#">EN</a>
+            </div>
+        </ul>
+    </div>
 </div>
 
 <!-- end Header -->
-
